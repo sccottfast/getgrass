a9deaea067ff8a2d2282013672da5f17344e713a
scott585 <sccottfast@gmail.com>
2024-02-25 16:09:47 +0800

    no message

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
-
-
 cd getgrass/
 user_id=$1
 ip=$2
 echo "user_id: $user_id"
 echo "ip: $ip"
 ps -ef | grep "main.py"| grep -v grep | awk '{print $2}'| xargs kill -9
-
-nohup python3 main.py $user_id $ip  > main.log &
+nohup python3 -u /root/getgrass/main.py $user_id $ip  > my.out 2>&1 &
