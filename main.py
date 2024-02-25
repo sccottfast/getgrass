82a68fb802d1351e445921d709db2db2681fc5e4
scott585 <sccottfast@gmail.com>
2024-02-25 14:10:31 +0800

    no message

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def main():
     print('代理',proxy)
    
     # TODO 修改代理列表
-    socks5_proxy_list =proxy.split(",",1)
+    socks5_proxy_list =proxy.split(",",20)
     print('代理',socks5_proxy_list)
     
     tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in socks5_proxy_list]
