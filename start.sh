aede79bf7d9fa3bd97145d3df3ba1ae9fd5f7041
scott585 <sccottfast@gmail.com>
2024-02-25 14:26:44 +0800

    no message

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ user_id=$1
 ip=$2
 echo "user_id: $user_id"
 echo "ip: $ip"
+ps -ef | grep \"main.py\"| grep -v grep | awk '{print $2}'| xargs kill -9
 nohup python3 main.py $user_id $ip  > grass.log &
