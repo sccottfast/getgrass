55413955f7f90b0a7eae06306c969d0b293f833e
scott585 <sccottfast@gmail.com>
2024-02-25 15:34:14 +0800

    no message

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ echo "user_id: $user_id"
 echo "ip: $ip"
 ps -ef | grep \"main.py\"| grep -v grep | awk '{print $2}'| xargs kill -9
 
-nohup python3 main.py $user_id $ip    >> main.log &
+nohup python3 main.py $user_id $ip  
