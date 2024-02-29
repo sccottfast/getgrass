c32acec81b832fa8a8cdb80bb533c1e717dd6214
scott585 <sccottfast@gmail.com>
2024-03-01 02:14:22 +0800

    no message

--- start_clone.sh
+++ start_clone.sh
@@ -5,4 +5,4 @@ proxy_num=$3
 echo "user_id: $user_id"
 echo "ip: $ip"
 ps -ef | grep "my_clone.py"| grep -v grep | awk '{print $2}'| xargs kill -9
-nohup python3 -u /root/getgrass/main.py $user_id $ip $proxy_num > my_clone.out 2>&1 &
+nohup python3 -u /root/getgrass/main_clone.py $user_id $ip $proxy_num > my_clone.out 2>&1 &
