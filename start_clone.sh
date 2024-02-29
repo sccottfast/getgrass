a42192dab58b31ce8d06837904941139111e2aeb
scott585 <sccottfast@gmail.com>
2024-03-01 01:37:51 +0800

    双跑

---
+++ start_clone.sh
@@ -0,0 +1,8 @@
+cd getgrass/
+user_id=$1
+ip=$2
+proxy_num=$3
+echo "user_id: $user_id"
+echo "ip: $ip"
+ps -ef | grep "my_clone.py"| grep -v grep | awk '{print $2}'| xargs kill -9
+nohup python3 -u /root/getgrass/main.py $user_id $ip $proxy_num > my_clone.out 2>&1 &
