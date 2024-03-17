9bcc2fbdb97114f61f072c07db313175d00a663f
scott585 <sccottfast@gmail.com>
2024-03-17 21:39:14 +0800

    no message

---
+++ start_clone_3.sh
@@ -0,0 +1,8 @@
+cd getgrass/
+user_id=$1
+ip=$2
+proxy_num=$3
+echo "user_id: $user_id"
+echo "ip: $ip"
+ps -ef | grep "main_clone_3.py"| grep -v grep | awk '{print $2}'| xargs kill -9
+nohup python3 -u /root/getgrass/main_clone_3.py $user_id $ip $proxy_num > main_clone_3.out 2>&1 &
